be13b54a7a23926d7fa691791a3c8370ab459783
Oleksii Drenov <leshadrenev@gmail.com>
2025-04-22 23:09:19 +0300

    solution 5

--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
 def copy_file(command: str) -> None:
-    if len(command.split()) == 3:
-        file_copy = command.split()[-1]
-        file_original = command.split()[-2]
-        if file_original != file_copy:
-            try:
-                with (open(file_original, "r") as file_in,
-                      open(file_copy, "w") as file_out):
-                    content = file_in.read()
-                    file_out.write(content)
-            except FileNotFoundError as e:
-                print(e)
+    if len(command.split()) != 3 or command.split()[0] != "cp":
+        return
+    file_copy = command.split()[-1]
+    file_original = command.split()[-2]
+    if file_original == file_copy:
+        return
+    try:
+        with (open(file_original, "r") as file_in,
+                open(file_copy, "w") as file_out):
+            content = file_in.read()
+            file_out.write(content)
+    except FileNotFoundError as e:
+        print(e)
+    except PermissionError as e:
+        print(e)
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
